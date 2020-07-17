f654a4f38708d85499ebc32c33171673e082cdd0
erfannayeb <68284841+erfannayeb@users.noreply.github.com>
2020-07-17 13:27:35 +0430

    Update Board.java
    aaa

--- Quoridor/src/game/Board.java
+++ Quoridor/src/game/Board.java
@@ -1,4 +1,23 @@
 package game;
 
 public class Board {
+  private Boolean[][] board=new Boolean[9][9];
+        public Board(){
+            board[0][4]=true;
+            board[8][4]=true;
+        }
+        public void move(String playermove){
+            if(playermove.equalsIgnoreCase("Up")) {
+            }
+
+            if(playermove.equalsIgnoreCase("Down")){
+
+            }
+            if (playermove.equalsIgnoreCase("Right")){
+
+            }
+            if (playermove.equalsIgnoreCase("Left")){
+
+            }
+        }
 }
