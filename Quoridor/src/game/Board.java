44bae855f0a9d96bcf1d4006366d4b2f362eb491
erfannayeb <68284841+erfannayeb@users.noreply.github.com>
2020-07-18 07:03:02 +0430

    Update Board.java
    new aaa

--- Quoridor/src/game/Board.java
+++ Quoridor/src/game/Board.java
@@ -1,23 +1,41 @@
 package game;
 
-public class Board {
-  private Boolean[][] board=new Boolean[9][9];
-        public Board(){
-            board[0][4]=true;
-            board[8][4]=true;
-        }
+public class Board{
+        private Boolean[][] board=new Boolean[9][9];
+        Bead bead1=new Bead();
+        Bead bead2=new Bead();
+    public Board(){
+        board[0][4]=true;
+        board[8][4]=true;
+        bead1.setX(0);
+        bead1.setY(4);
+        bead2.setX(8);
+        bead2.setY(4);
+    }
         public void move(String playermove){
             if(playermove.equalsIgnoreCase("Up")) {
+                board[bead1.getX()][bead1.getY()]=false;
+                bead1.setX(bead1.getX()-1);
+                board[bead1.getX()][bead1.getY()]=true;
             }
 
             if(playermove.equalsIgnoreCase("Down")){
-
+                board[bead1.getX()][bead1.getY()]=false;
+                bead1.setX(bead1.getX()+1);
+                board[bead1.getX()][bead1.getY()]=true;
             }
             if (playermove.equalsIgnoreCase("Right")){
-
+                board[bead1.getX()][bead1.getY()]=false;
+                bead1.setY(bead1.getY()+1);
+                board[bead1.getX()][bead1.getY()]=true;
             }
             if (playermove.equalsIgnoreCase("Left")){
-
+                board[bead1.getX()][bead1.getY()]=false;
+                bead1.setY(bead1.getY()-1);
+                board[bead1.getX()][bead1.getY()]=true;
             }
         }
-}
+
+    }
+
+
