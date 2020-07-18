9bb8008cb9ffc35c8a0ab87021604fa1387130eb
Adjective1380 <mm.80xefatzadeh@gmail.com>
2020-07-18 22:27:45 +0430

    A little further changes

--- Quoridor/src/graphics/Controller.java
+++ Quoridor/src/graphics/Controller.java
@@ -1,4 +1,19 @@
 package graphics;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+	@FXML
+	private Button NewGame = new Button();
+	@FXML
+	private Button LoadGame = new Button();
+	@FXML
+	private Button Settings = new Button();
+
+	private Play play;
+
+	public void setPlay(Play play) {
+		this.play = play;
+	}
 }
--- Quoridor/src/graphics/Play.java
+++ Quoridor/src/graphics/Play.java
@@ -8,14 +8,15 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Play extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
-        Scene scene = new Scene(root, 907, 600);
+        Parent menu = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+//        Parent game = FXMLLoader.load(getClass().getResource("game.fxml"));
+        Scene MainMenu = new Scene(menu, 907, 600);
+//        Scene Game = new Scene(game, 900, 600);
         primaryStage.setTitle("Quoridor Game");
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("icon.jfif")));
-        primaryStage.setScene(scene);
+        primaryStage.setScene(MainMenu);
         primaryStage.show();
     }
 
