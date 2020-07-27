9feb20b0ab45e99cd0b7a0d7d9781902c3e99a7d
erfannayeb <68284841+erfannayeb@users.noreply.github.com>
2020-07-27 19:47:48 +0430

    Update Bead.java

--- Quoridor/src/game/Bead.java
+++ Quoridor/src/game/Bead.java
@@ -4,18 +4,13 @@ public class Bead {
     private int x;
     private int y;
 
-    public void setX(int x) {
-        this.x = x;
-    }
-    public int getX() {
-        return x;
+    public Bead(int x, int y) {
+        this.setX(x);
+        this.setY(y);
     }
 
-    public void setY(int y) {
-        this.y = y;
-    }
-    public int getY() {
-        return y;
-    }
-
+    public int getX() { return x; }
+    public void setX(int x) { this.x = x; }
+    public int getY() { return y; }
+    public void setY(int y) { this.y = y; }
 }
