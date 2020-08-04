34cd5443ca8dc1bbd17f66a147141706cca054c0
erfannayeb <68284841+erfannayeb@users.noreply.github.com>
2020-08-04 14:15:29 +0430

    Update Player.java

--- Quoridor/src/game/Player.java
+++ Quoridor/src/game/Player.java
@@ -1,23 +1,25 @@
-package game;
+ package game;
 
-public class Player {
-    char id;
-    String name = "No name";
-    Bead bead;
-    //int walls = 10;
+    public class Player {
+        char id;
+        String name = "No name";
+        Bead bead;
+        int walls = 10;
+        public boolean skip_level = false;
+        public char getId() { return this.id; }
+        public String getName() { return this.name; }
+        public Bead getBead() { return this.bead; }
+        public int getWalls() { return this.walls; }
+        public void setBead(int y, int x) { this.bead = new Bead(y, x); }
+        public void decreaseWalls() { this.walls--; }
 
-    public char getId() { return id; }
-    public String getName() { return name; }
-    public Bead getBead() { return bead; }
-    //public int getWalls() { return walls; }
-    public void setBead(int y, int x) { this.bead = new Bead(y, x); }
-
-    public Player(char id) {
-        this.id = id;
-        if (id == 'U')
-            this.bead = new Bead(0, 8);
-        else
-            this.bead = new Bead(16, 8);
+        public Player(String name, char id, int walls) {
+            this.name = name;
+            this.walls = walls;
+            this.id = id;
+            if (id == 'U')
+                this.bead = new Bead(0, 8);
+            else
+                this.bead = new Bead(16, 8);
+        }
     }
-    public Player(String name, int walls) { this.name = name; /*this.walls = walls;*/ }
-}
