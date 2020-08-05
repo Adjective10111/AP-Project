401a627c3cffdf5d5c915fe5874c5e9e82b63451
Adjective1380 <mm.80xefatzadeh@gmail.com>
2020-08-05 10:07:31 +0430

    read comments

--- Quoridor/src/game/Cup.java
+++ Quoridor/src/game/Cup.java
@@ -2,24 +2,25 @@ package game;
 import java.util.*;
 
 public class Cup {
-    private int playercount;
-    private int level = 1;
     private ArrayList<Player> players = new ArrayList<Player>();
     private int index = -2;
-    public Cup(ArrayList players) {
 
+    public Cup(ArrayList players) {
+        // in kojas? chera khalie?:\
     }
-    public Player[] play(ArrayList<Player> players){
-        if (players.size() % 2 != 0){
+
+    public Player[] play(ArrayList<Player> players) {
+        if (players.size() % 2 != 0) {
             index += 2;
-                return new Player[]{players.get(index + 1), players.get(index + 2)};
-            }
-        else{
-            index += 2;
-                return new Player[]{players.get(index), players.get(index + 1)};
-            }
+            return new Player[] {players.get(index + 1), players.get(index + 2)};
         }
-    public void won(int winnerNumber){
+        else {
+            index += 2;
+            return new Player[] {players.get(index), players.get(index + 1)};
+        }
+    }
+
+    public void won(int winnerNumber) {
         if (players.size() % 2 != 0) {
             if (winnerNumber == 1)
                 players.remove(index + 2);
@@ -31,22 +32,24 @@ public class Cup {
             else players.remove(index);
         }
     }
-    public void nextLevel(){
+
+    public void nextLevel() {
         Collections.shuffle(players);
         if (players.size() % 2 != 0)
-              players.get(0).skip_level = true;
+            players.get(0).skip_level = true;
         index = -2;
     }
-    public boolean levelFinished(){
-        if (index >= players.size()){
-             return true;
+
+    public boolean levelFinished() {
+        if (index >= players.size()) {
+            return true;
         }
         else return false;
     }
-    public Player finished(){
+
+    public Player finished() {
         if (players.size() == 1)
             return players.get(0);
         else return null;
     }
-
 }
